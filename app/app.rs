ad3e266448fc80ddf3f50cc6048d34e7e5c3a996
octobocto <bo@jalborg.com>
2026-09-09 12:03:58 -0700

    app: do not stop the wallet task on an error

--- app/app.rs
+++ app/app.rs
@@ -124,12 +124,16 @@ impl App {
     ) -> Result<(), Error> {
         let mut state_changes = node.watch_state();
         while let Some(()) = state_changes.next().await {
-            let () = update(
+            let update_result = update(
                 &node,
                 &mut utxos.write(),
                 &mut unconfirmed_utxos.write(),
                 &wallet,
-            )?;
+            );
+            if let Err(err) = update_result {
+                let err = anyhow::Error::from(err);
+                tracing::warn!("Failed to update wallet: {err:#}");
+            }
         }
         Ok(())
     }
